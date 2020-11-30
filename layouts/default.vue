deccf6ada787c7ffcccdf321ce00873dfcd45495
Charlie Robinson <45928293+charliedr@users.noreply.github.com>
2020-11-30 16:05:57 -0600

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,8 @@ export default {
   head() {
     return {
       meta: [
+        { hid: 'og:url', property: 'og:url', content: 'https://eatlocalobky.com' },
+        { hid: 'og:type', content: 'website' },
         { hid: 'og:title', property: 'og:title', content: 'Eat Local OBKY' },
         { hid: 'og:description', property: 'og:description', content: 'A directory for locally owned restaurants in the Owensboro, KY area.' },
         { hid: 'og:image', property: 'og:image', content: 'ogImage.jpg' }
